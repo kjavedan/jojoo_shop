fbe3f115ae620be5746ffbc9018f026d0e0053c0
Khaled Javedan <khaledjavedan76@gmail.com>
2023-10-24 12:12:01 +0400

    Wroking on stepper

--- src/components/Stepper.vue
+++ src/components/Stepper.vue
@@ -3,38 +3,58 @@
     <div class="steps">
       <div
         v-for="(step, index) in steps"
-        :key="step"
+        :key="index"
         @click="selectStep(index)"
         :class="['step', { active: index === status }]"
       >
         <div class="top">
           <div class="step-circle">
-            <i class="icon"></i>
+            <img :src="step.icon" class="icon" />
           </div>
         </div>
 
-        <div class="step-label">{{ step }}</div>
+        <div class="step-label">{{ step.title }}</div>
       </div>
     </div>
+
     <div class="step-info" v-if="currentStep !== null">
-      {{ stepInfo[currentStep] }}
+      {{ steps[currentStep].info }}
     </div>
   </div>
 </template>
 
 <script setup>
 import { onBeforeMount, ref } from 'vue'
+import inProcessIcon from '@/assets/images/Process.png'
+import packedIcon from '@/assets/images/Packed.png'
+import onTheWayIcon from '@/assets/images/On-the-way.png'
+import deliveredIcon from '@/assets/images/Shipped.png'
 
 // props
 const props = defineProps(['status'])
 
 // static
-const steps = ['In Process', 'Packed', 'On the Way', 'Delivered']
-const stepInfo = [
-  'This step represents the initial processing of your order.',
-  'Your items are packed and ready for shipment.',
-  'Your order is on the way to your delivery address.',
-  'Congratulations! Your order has been successfully delivered.'
+const steps = [
+  {
+    title: 'In Process',
+    info: 'This step represents the initial processing of your order.',
+    icon: inProcessIcon
+  },
+  {
+    title: 'Packed',
+    info: 'Your items are packed and ready for shipment',
+    icon: packedIcon
+  },
+  {
+    title: 'On the Way',
+    info: 'Your order is on the way to your delivery address.',
+    icon: onTheWayIcon
+  },
+  {
+    title: 'Delivered',
+    info: 'Congratulations! Your order has been successfully delivered..',
+    icon: deliveredIcon
+  }
 ]
 
 // ref
@@ -63,21 +83,34 @@ onBeforeMount(() => {
 
 .steps {
   display: flex;
+  align-items: center;
   justify-content: space-between;
   width: 100%;
   margin-bottom: 20px;
 
   .step {
-    border: solid green;
     @include flex-col;
     align-items: center;
-  }
-
-  .top {
-    border: solid;
-  }
-  .line {
-    border: solid;
+    width: 25%;
+    .top {
+      border: solid;
+      position: relative;
+      &::before {
+        content: '';
+        position: absolute;
+        top: 50%;
+        left: 0%;
+        width: calc(20svw - 34px);
+        height: 2px;
+        background-color: #ccc;
+        transform: translate(-100%, -50%);
+      }
+    }
+    &:nth-child(1) {
+      ::before {
+        display: none;
+      }
+    }
   }
 }
 
@@ -89,27 +122,29 @@ onBeforeMount(() => {
 }
 
 .step-circle {
-  width: 30px;
-  height: 30px;
-  border: 2px solid #4caf50;
+  width: 34px;
+  height: 34px;
+  background: #ededed;
   border-radius: 50%;
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-bottom: 5px;
+  padding-top: 5px;
+  box-shadow: 2px 2px 7px 0px rgba(206, 206, 206, 0.43) inset;
+  @include flex-center;
 
   .icon {
-    color: #4caf50;
+    height: 24px;
   }
 }
 
 .step-label {
-  font-size: 14px;
+  font-size: 11px;
   text-align: center;
 }
 
 .steps div.active .step-circle {
-  background-color: #4caf50;
+  background-color: #000000;
   border: none;
 
   .icon {
