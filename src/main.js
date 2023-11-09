06165545458d488f873c1e5425beb65e20af80a7
Khaled Javedan <khaledjavedan76@gmail.com>
2023-11-09 20:46:48 +0400

    woring on login page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './assets/scss/main.scss'
-
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -10,4 +11,5 @@ import router from './router'
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 app.mount('#app')
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,22 +1,107 @@
 <template>
   <div class="login">
     <h2>login to your account</h2>
-  </div>
-
-  <div class="with-goole">
-    <img src="@/assets/images/goole.png" alt="google" />
-    <span>Google</span>
-  </div>
-  <div class="or-with">Or with email and password</div>
-  <form></form>
-  <div class="no-account">
-    Don't have an account?
-    <RouterLink :to="{ name: 'sign-up' }"> Sign Up </RouterLink>
+    <div class="with-google">
+      <img src="@/assets/images/google.png" alt="google" />
+      <span>Google</span>
+    </div>
+    <div class="or-with-line">
+      <div class="line"></div>
+      <span class="or-text">Or </span>
+      <div class="line"></div>
+    </div>
+    <el-form
+      ref="loginFormRef"
+      :model="loginForm"
+      :rules="rules"
+      class="login-form"
+      label-position="top"
+      status-icon
+    >
+      <el-form-item label="Username" prop="username">
+        <el-input
+          type="text"
+          v-model="loginForm.username"
+          placeholder="enter your username"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="Password" prop="password">
+        <el-input
+          type="password"
+          v-model="loginForm.password"
+          placeholder="enter your password"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitLoginForm(loginFormRef)"> Create </el-button>
+      </el-form-item>
+    </el-form>
+    <div class="no-account">
+      Don't have an account?
+      <RouterLink :to="{ name: 'sign-up' }"> Sign Up </RouterLink>
+    </div>
   </div>
 </template>
 
-<script>
-export default {}
+<script setup>
+import { ref } from 'vue'
+
+//refs
+const loginFormRef = ref(null)
+const loginForm = ref({ username: '', password: '' })
+const rules = ref({
+  username: [{ required: true, message: 'Please enter username', trigger: 'blur' }],
+  password: [{ required: true, message: 'plase enter your password', trigger: 'blur' }]
+})
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import '@/assets/scss/main.scss';
+
+.login {
+  border: solid;
+
+  .with-google {
+    background: #f6f6f6;
+    width: 100%;
+    height: 45px;
+    @include round-m;
+    @include shadow-m;
+    @include flex-center;
+    gap: 10px;
+    margin-top: 2rem;
+    span {
+      font-weight: 800;
+    }
+    @include ease;
+    &:active {
+      scale: 0.9;
+    }
+    img {
+      height: 70%;
+    }
+  }
+
+  .or-with-line {
+    display: flex;
+    align-items: center;
+    text-align: center;
+    margin: 2rem 0; /* Adjust margin as needed */
+  }
+
+  .line {
+    flex: 1;
+    height: 1px;
+    background-color: #b9b9b9;
+  }
+
+  .or-text {
+    color: #868686;
+    margin: 0 10px; /* Adjust margin as needed */
+  }
+
+  .no-account {
+    text-align: center;
+  }
+}
+</style>
