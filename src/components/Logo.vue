977eab25c9aadfd4e1106ab9a3b31c00327f073b
Khaled Javedan <khaledjavedan76@gmail.com>
2023-11-15 12:02:06 +0400

    Working on product page

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="header">
+  <header :class="['header', { 'is-product': isProduct }]">
     <div class="logo" v-if="isHome">
       <RouterLink :to="{ name: 'home' }">
         <h1>jojoo <span>shop.</span></h1>
@@ -30,10 +30,12 @@ const router = useRouter()
 
 //refs
 const isHome = ref(false)
+const isProduct = ref(false)
 
 //hooks
 watch(route, () => {
   route.name === 'home' ? (isHome.value = true) : (isHome.value = false)
+  route.name === 'product' ? (isProduct.value = true) : (isProduct.value = false)
 })
 </script>
 
@@ -49,6 +51,24 @@ watch(route, () => {
     rgba(255, 255, 255, 0) 77.19%,
     #fff 100%
   );
+
+  &.is-product {
+    background: transparent;
+    position: fixed;
+    // border: solid;
+    width: 98%;
+    left: 50%;
+    transform: translateX(-50%);
+    top: 1%;
+    @include pc-s {
+      width: 80%;
+    }
+
+    @include pc-xl {
+      width: 1400px;
+    }
+  }
+
   top: -2px;
   left: 0;
   width: 100%;
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,9 +1,98 @@
 <template>
-  <h1 class="page-title">product</h1>
+  <div class="slides-wrapper" :style="{ height: slidesWrapperHeight }">
+    <div class="slides">image will go here</div>
+  </div>
+  <div class="product-info">
+    <div class="header">
+      <h2 class="header-title">Pokemon</h2>
+      <span class="rate">rate</span>
+      <span class="price">290.99AED</span>
+    </div>
+    <div class="body">
+      <h4 class="body-title">description</h4>
+      <p class="body-description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, amet adipisci dicta,
+        laudantium iure provident dignissimos maiores delectus eos tempora libero et similique
+        voluptates odit laborum eaque optio vitae incidunt!
+      </p>
+    </div>
+    <div class="footer">
+      <h4 class="footer-title">Reviews</h4>
+      <div class="review-item">review Item</div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {}
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+
+const slidesWrapperHeight = ref('25rem')
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
+
+const handleScroll = () => {
+  const scrollPosition = window.scrollY
+
+  // Adjust the height based on the scroll position
+  slidesWrapperHeight.value = scrollPosition > 10 ? '18rem' : '25rem'
+}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import '@/assets/scss/main.scss';
+.slides-wrapper {
+  border: solid;
+  position: sticky;
+  top: 0;
+  height: 25pc;
+  background: #f6f6f6;
+  @include flex-center;
+  transition: height 0.2s ease;
+  .slides {
+    border: solid;
+    height: 90%;
+    width: 90%;
+  }
+}
+.product-info {
+  margin-top: 1rem;
+  border: solid;
+
+  .header {
+    display: flex;
+    align-items: center;
+    .rate {
+      margin-left: 5px;
+    }
+    .price {
+      margin-left: auto;
+    }
+  }
+  .body {
+    margin-top: 1rem;
+    border: solid;
+    .body-title {
+      font-weight: bold;
+    }
+    .description {
+      margin-top: 0.5rem;
+      font-size: 0.8rem;
+      line-height: 0.8rem;
+      color: #717171;
+    }
+  }
+  .footer {
+    margin-top: 1rem;
+    border: solid;
+    .footer-title {
+      font-weight: bold;
+    }
+  }
+}
+</style>
